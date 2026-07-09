828fd239502f75166b8b57d8cfe6ca322003cc9f
shantoopaul <shantoopaul@gmail.com>
2026-07-09 15:42:47 +0600

    add: stripe setup configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -13,5 +13,7 @@ export default {
     jwt_access_secret: process.env.JWT_ACCESS_SECRET,
     jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
     jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN as StringValue,
-    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as StringValue
+    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as StringValue,
+    stripe_product_id: process.env.STRIPE_PRODUCT_ID,
+    stripe_secret_key: process.env.STRIPE_SECRET_KEY
 }
